3b63960c00d2086f9645a25731b11c52c7ced478
TJ Draper <tj@caddis.co>
2015-10-26 13:55:58 -0500

    Add support for ExpressionEngine 3

--- system/expressionengine/third_party/math/pi.math.php
+++ system/expressionengine/third_party/math/pi.math.php
@@ -1,11 +1,13 @@
 <?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 
+include_once(PATH_THIRD . 'math/addon.setup.php');
+
 $plugin_info = array (
-	'pi_name' => 'Math',
-	'pi_version' => '1.4.1',
-	'pi_author' => 'Caddis',
-	'pi_author_url' => 'http://www.caddis.co',
-	'pi_description' => 'Use Math to execute PHP supported math formulas.',
+	'pi_author' => MATH_AUTHOR,
+	'pi_author_url' => MATH_AUTHOR_URL,
+	'pi_description' => MATH_DESC,
+	'pi_name' => MATH_NAME,
+	'pi_version' => MATH_VER,
 	'pi_usage' => Math::usage()
 );
 
@@ -83,7 +85,7 @@ class Math {
 							break;
 	   					default:
 	   						$result = intval($result * $mult) / $mult;
-					} 
+					}
 				}
 
 				$parts = explode('.', $result);
@@ -115,32 +117,6 @@ class Math {
 
 	public static function usage()
 	{
-		ob_start();
-?>
-Parameters:
-
-formula = '(5 * 2) / [1]' // math formula (required) supports the following operators as well as bitwise + - * / % ++ -- < > <= => != <> ==
-params = '{var}|{var2}'   // pipe delimited list of numeric parameters to be replaced into formula, recommended due to use of PHP eval (default: null)
-decimals = '2'            // sets the number of decimal points (default: "0")
-decimal_point = '.'       // sets the separator for the decimal point (default: ".")
-thousands_seperator = ',' // sets the thousands separator; (default: ",")
-absolute = 'yes'          // return the absolute number of the result (defaults: "no")
-round = 'up|down|ceil'    // whether to round the result up or down, where up is standard rounding (defaults: no rounding)
-numeric_error = 'Error'   // message returned when non-numeric parameters are provided (default: "Invalid input")
-trailing_zeros = 'yes'    // include trailing 0 decimal places (defaults: "no")
-
-Usage:
-
-{exp:math formula="10 - 12" absolute="yes"} outputs 2
-{exp:math formula="((4 * 5) / 2)" decimals="2"} outputs 10.00
-{exp:math formula="([1] + 1) / [2]" params="{total_results}|2" round="down"} outputs 5 where {total_results} is 10
-{exp:math formula="2/3" decimals="2" round="up"} outputs 0.67
-<?php
-		$buffer = ob_get_contents();
-
-		ob_end_clean();
-
-		return $buffer;
+		return 'See docs and examples at https://github.com/caddis/math';
 	}
 }
-?>
